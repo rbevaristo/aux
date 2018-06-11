d815ea9fdc5bc8741c3578ccd76be2884c4c7d22
unknown <richard120189@gmail.com>
2018-06-11 21:11:58 +0800

    added password-reset

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -3,10 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Symfony\Component\HttpFoundation\Response;
+Use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class ResetPasswordController extends Controller
 {
-    public function sendEmail(){
+    public function sendEmail(Request $request) {
+        if(!$this->validateEmail($request->email)){
+            return $this->failedResponse();
+        }
 
+        $this->send($request->email);
+        return $this->successResponse();
     }
+
+    public function validateEmail($email) {
+        return !!User::where('email', $email)->first();
+    }
+
+    
+    public function successResponse() {
+        return response()->json([
+            'data' => 'Reset Email is send successfully, please check your inbox.'
+        ], Response::HTTP_OK);
+    }
+
+    public function failedResponse(){
+        return response()->json([
+            'error' => 'Email doesn\'t exist on our database'
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    public function send($email) {
+        $token = $this->createToken($email);
+        Mail::to($email)->send(new ResetPasswordMail($token));
+    }
+
+    public function createToken($email) {
+        $oldToken = DB::table('password_resets')->where('email', $email)->first();
+        if($oldToken){
+            return $oldToken;
+        }
+        $token = str_random(60);
+        $this->saveToken($token, $email);
+        return $token;
+    }
+
+    public function saveToken($token, $email) {
+        DB::table('password_resets')->insert([
+            'email' => $email,
+            'token' => $token,
+            'created_at' => Carbon::now(),
+        ]);
+    }
+
 }
